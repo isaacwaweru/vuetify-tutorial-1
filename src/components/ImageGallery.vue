b2e9ab53d561bad6e1516bf48107234c7e3561ac
Isaac Waweru <waweruisaac25@gmail.com>
2024-12-10 18:12:57 +0300

    initial commit

---
+++ src/components/ImageGallery.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps(["isWithColor"]);
+
+const copyUrl = async (url) => {
+  await navigator.clipboard.writeText(url);
+};
+</script>
+
+<template>
+  <VCard class="mx-5 my-2 pa-3">
+    <VRow>
+      <VCol v-for="n in 204" :key="n" cols="4" sm="3" md="2" lg="1">
+        <VCard
+          @click="
+            copyUrl(
+              `https://picsum.photos/500/300?image=${n * 5 + 10}${
+                props.isWithColor ? '' : '&grayscale'
+              }`
+            )
+          "
+        >
+          <VImg
+            :src="`https://picsum.photos/500/300?image=${n * 5 + 10}${
+              props.isWithColor ? '' : '&grayscale'
+            }`"
+            :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}${
+              props.isWithColor ? '' : '&grayscale'
+            }`"
+            aspect-ratio="1"
+            cover
+          >
+            <template v-slot:placeholder>
+              <VRow align="center" class="fill-height ma-0" justify="center">
+                <VProgressCircular
+                  color="grey-lighten-5"
+                  indeterminate
+                ></VProgressCircular>
+              </VRow>
+            </template>
+          </VImg>
+        </VCard>
+      </VCol>
+    </VRow>
+  </VCard>
+</template>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { defineEmits } from "vue";
+
+const emits = defineEmits(["selectOption"]);
+
+const selectOption = (isColor) => {
+  emits("selectOption", isColor);
+};
+</script>
+
+<template>
+  <VNavigationDrawer expand-on-hover rail>
+    <VList>
+      <VListItem
+        prepend-avatar="https://kg-farmers-s3.lon1.digitaloceanspaces.com/profile/8f100837-016b-4e7c-bb96-6cc0ba023ebe.jpg"
+        subtitle="cb@gmailcom"
+        title="Chris Brown"
+      ></VListItem>
+    </VList>
+
+    <VDivider />
+
+    <VList>
+      <VListItem
+        prepend-icon="mdi-image"
+        title="Color Gallery"
+        value="color"
+        @click="selectOption(true)"
+      />
+      <VListItem
+        prepend-icon="mdi-water"
+        title="Grayspace Gallery"
+        value="gray"
+        @click="selectOption(false)"
+      />
+    </VList>
+  </VNavigationDrawer>
+</template>
